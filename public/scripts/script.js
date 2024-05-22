01a8d8deeb6d484c8fd3721bf48a79a45f7bfd46
Himantha Marasinghe <himanthamarasinghe@gmail.com>
2024-05-22 11:03:53 +0530

    Add some js functionalities and more.
    Add 2 forms to get income items and expenses items separately.
    Add js functionalities to toggle between the 2 forms.
    Add balance div to show the balance of the user.

---
+++ public/scripts/script.js
@@ -0,0 +1,9 @@
+function addIncome() {
+    document.getElementById('addExpense').classList.remove('active');
+    document.getElementById('addIncome').classList.add('active');
+}
+
+function addExpense() {
+    document.getElementById('addIncome').classList.remove('active');
+    document.getElementById('addExpense').classList.add('active');
+}
